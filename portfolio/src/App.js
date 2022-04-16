0a65628f3e9ba56ce968249ce74aaea6f7f3b526
Sloan <Sloan.boyce@gmail.com>
2022-04-16 18:32:48 -0400

    added incomplete nav bar. Attempting to figure out some designs

---
+++ portfolio/src/App.js
@@ -0,0 +1,12 @@
+import Nav from './components/Nav';
+import './index.css'
+
+function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Nav.js
@@ -0,0 +1,12 @@
+
+const Nav = () => {
+    return (
+        <div className="top-0 left-0 w-screen h-15 bg-gray-500 text-white flex flex-row">
+
+            <div className="p-3 m-4 hover:bg-gray-700 hover:cursor-pointer">Works</div>
+            <div className="p-3 m-4 hover:bg-gray-700 hover:cursor-pointer">About</div>
+        </div>
+    )
+};
+
+export default Nav;
