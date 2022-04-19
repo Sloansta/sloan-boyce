4733c405d4f2e019f72945633f20125be2e26da7
Sloan <Sloan.boyce@gmail.com>
2022-04-18 23:41:49 -0400

    added about section title

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,13 @@
 //import Nav from './components/Nav';
 import Jumbotron from './components/Jumbotron';
+import About from './components/About';
 import './index.css'
 
 function App() {
   return (
-    <div>
+    <div className='smooth-scroll md:scroll-auto'>
       <Jumbotron />
+      <About />
     </div>
   );
 }
---
+++ portfolio/src/components/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+        <div className="flex flex-col bg-black text-white">
+            <span className="text-4xl md:justify-start sm:justify-center text-center p-4 m-5">Who is <strong>Sloan?</strong></span>
+        </div>
+    )
+};
+
+export default About;
