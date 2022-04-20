e7495a5279e20afe4be237d1318d26e87d540779
Sloan <Sloan.boyce@gmail.com>
2022-04-19 21:54:53 -0400

    added animations

--- portfolio/src/components/Jumbotron.js
+++ portfolio/src/components/Jumbotron.js
@@ -8,8 +8,9 @@ const Jumbotron = () => {
             src={Image} 
             alt="Osaka" 
             />
-            <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.9)] text-center flex-1 flex items-center justify-center flex-col">
-                <h1 className="mb-6 text-4xl xl:text-5xl">Hi, my name is Sloan👋</h1>
+            <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.9)] text-center flex-1 
+            flex items-center justify-center flex-col animate-fade-in-down">
+                <h1 className="mb-6 text-4xl xl:text-5xl">Hi, my name is Sloan<div className="animate-wiggle inline-block">👋</div></h1>
                 <h2 className="mb-2 text-2xl xl:text-3xl tracking-tight">I'm a fullstack developer based in Michigan</h2>
             </div>
         </div>
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -3,7 +3,32 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          }
+        },
+        'wiggle': {
+          '0%, 100%': {
+            transform: 'rotate(-3deg)'
+          },
+          '50%': {
+            transform: 'rotate(3deg)'
+          }
+        }
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-out',
+        'wiggle': 'wiggle 1s ease-in-out infinite'
+      }
+    },
   },
   plugins: [],
 }
