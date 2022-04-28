36792d9cbf20bdc0a5014a7666612748f1e20f83
Sloan <Sloan.boyce@gmail.com>
2022-04-27 20:08:04 -0400

    added some custom CSS to see if that would resolve the white bar issue

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,7 +7,7 @@ import './index.css'
 
 function App() {
   return (
-    <div className='py-0 my-0 smooth-scroll overflow-hidden md:scroll-auto'>
+    <div className='py-0 my-0 smooth-scroll overflow-hidden w-full h-full overflow-x-hidden md:scroll-auto'>
       <Jumbotron />
       <About />
       <Works />
